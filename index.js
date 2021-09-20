b463c5d19660690462fa3f670ac1a419b9c72d13
Leo Chen <chenl3@rose-hulman.edu>
2021-09-20 13:11:01 -0400

    commit tree f0b3bae27557f4d78fd3418ad3be7ef762dfb664

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if(someValue <= 400){
+    return 'This one is on me!';
+  } else if (someValue > 2500){
+    return 'No can do.';
+  } else{
+    return 'I will gladly take your thirty bucks.';
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity){
   // Write your code here!
+  // if(someCity === 'NYC'){
+  //   return 'Ok, sounds good.';
+  // } else{
+  //   return "No go.";
+  // }
+  return someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someTip){
   // Write your code here!
+  switch(someTip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
